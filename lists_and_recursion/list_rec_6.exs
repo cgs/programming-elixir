f31f643a66b3c6ba820b1981711d70a46370e987
Chris Sepic <chris.sepic@gmail.com>
2016-06-29 18:03:53 -0700

    remove extraneous match

--- lists_and_recursion/list_rec_6.exs
+++ lists_and_recursion/list_rec_6.exs
@@ -1,6 +1,5 @@
 defmodule MyList do
   def flatten([]), do: []
-  def flatten([el]) when not is_list(el), do: [el]
   def flatten([h | t]) when not is_list(h), do: [h] ++ flatten(t)
   def flatten([h | t]), do: flatten(h) ++ flatten(t)
 end
